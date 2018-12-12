425c8236358731ad3512e5b0b0a2c685d9a7f2e1
Adrian Novegil <adrian.novegil@gmail.com>
2018-12-12 20:08:11 +0000

    Removed the cloud database configuration.

--- src/main/java/com/devcircus/adventurebox/uaa/UaaApp.java
+++ src/main/java/com/devcircus/adventurebox/uaa/UaaApp.java
@@ -50,10 +50,6 @@ public class UaaApp {
             log.error("You have misconfigured your application! It should not run "
                     + "with both the 'dev' and 'prod' profiles at the same time.");
         }
-        if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_CLOUD)) {
-            log.error("You have misconfigured your application! It should not "
-                    + "run with both the 'dev' and 'cloud' profiles at the same time.");
-        }
     }
 
     /**
